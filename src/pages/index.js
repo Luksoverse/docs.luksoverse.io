949187f70c10052508ff357a2448c04ca5384167
JordyDutch <85904689+JordyDutch@users.noreply.github.com>
2022-08-18 21:57:28 +0200

    change homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,18 @@
 import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Link from '@docusaurus/Link';
 
-import styles from './index.module.css';
+import styles from './index.module.scss';
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+function Index() {
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/the-guide/start-here">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
+    <Layout description="Set up your node and validators">
+      <div className={styles.container}>
+        <div
+          style={{
+            padding: '4rem 1rem',
+          }}
   );
 }
 
-export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
-}
+export default Index;
