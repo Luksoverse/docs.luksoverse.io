08f518678ab597ccb8fea3507883e830d1d35c61
JordyDutch <85904689+JordyDutch@users.noreply.github.com>
2022-08-18 21:49:14 +0200

    change layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,21 +9,6 @@ import styles from './index.module.css';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/the-guide/start-here">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
 }
 
 export default function Home() {
