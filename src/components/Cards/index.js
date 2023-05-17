80f81cb3f95dc6e11f8c5a3611cfe3f1b55b0a24
JordyDutch <85904689+JordyDutch@users.noreply.github.com>
2023-05-17 10:15:02 +0200

    Homepage upgrade

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -5,43 +5,28 @@ const Cards = () => {
   const cardData = [
     {
       title: 'Run a Node',
-      description: 'Learn how to setup a Lukso node from start to finish.',
+      description: 'Prepare your system, install your node and setup monitoring.',
       path: '/docs/node-guide/start-here',
     },
     {
-      title: "Johnny's Node Guide",
-      description: 'Setting up your system and start the LUKSO network.',
-      path: '/docs/community-guides/johnny-ebd-node-guide',
-    },
-    {
-      title: 'System & Monitoring',
-      description: 'How to setup your system and Grafana/Prometheus dashboard.',
-      path: '/docs/community-guides/system-monitoring-setup',
-    },
-    {
-      title: 'VPN Setup',
-      description: 'How to setup a VPN for external access to your node.',
-      path: '/docs/community-guides/vpn-setup',
-    },
-    {
-      title: 'OpenSSH Setup',
-      description: 'How to setup SSH for remote controls or data transfers.',
-      path: '/docs/community-guides/openssh-setup',
-    },
-    {
-      title: 'SSH Security',
-      description: 'Learn to improve your SSH setup for advanced security.',
-      path: '/docs/community-guides/ssh-security',
-    },
-    {
       title: 'Troubleshooting',
-      description: 'Learn to troubleshoot your node with easy tooltips.',
+      description: 'Troubleshoot your node with easy tooltips.',
       path: '/docs/community-guides/node-troubleshoot',
     },
     {
       title: 'Uninstalling',
       description: 'How to remove depricated node tools and their data.',
       path: '/docs/community-guides/node-uninstall',
+    },
+    {
+      title: 'Advanced Setup',
+      description: 'Community Guides how to use an advanced setup to acces your node externally.',
+      path: '/docs/community-guides/',
+    },
+    {
+      title: "Community Archive",
+      description: 'Setting up your system and start the LUKSO network.',
+      path: '/docs/community-guides/community-archive',
     },
     {
       title: 'Archive Docs',
