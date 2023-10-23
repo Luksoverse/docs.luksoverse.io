df51e05d66a0cea119c8e0f39ac2f731678d3579
JordyDutch <jordydutch1@gmail.com>
2023-10-23 09:21:15 +0200

    push

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -8,40 +8,40 @@ const Cards = () => {
       description:
         'A complete guide to prepare your system, install your node and setup monitoring by LUKSO dev Felix Hildebrandt.',
       path: '/docs/mainnet/complete-node-guide/hardware-build/start-here',
-      color: '#FF5733',
+      color: '#B3B6BA',
     },
     {
       title: 'Staking Services',
       description:
         'Still too complicated? Luksoverse has some great no-coding options for you.',
       path: '/docs/staking/staking-services',
-      color: '#FF5733',
+      color: '#B3B6BA',
     },
     {
       title: 'Uninstalling',
       description: 'How to remove depricated node tools and their data.',
       path: '/docs/community-guides/node-uninstall',
-      color: '#FF5733',
+      color: '#B3B6BA',
     },
     {
       title: 'Advanced Setup',
       description:
         'Community Guides how to use an advanced setup to acces your node externally.',
       path: '/docs/community-guides/',
-      color: '#FF5733',
+      color: '#D1D4D8',
     },
     {
       title: 'Community Archive',
       description: 'Setting up your system and start the LUKSO network.',
       path: '/docs/community-guides/community-archive',
-      color: '#FF5733',
+      color: '#D1D4D8',
     },
     {
       title: 'LUKSO Archive',
       description:
         'Guides and Specs for L16 and depricated LUKSO CLI versions.',
       path: '/docs/l16-archive-docs/parameters',
-      color: '#FF5733',
+      color: '#D1D4D8',
     },
   ];
 
